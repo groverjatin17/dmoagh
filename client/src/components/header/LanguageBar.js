a4cc1680d1b978f7332f9ce045f65bf4171c87f4
groverjatin17 <groverjatin17@gmail.com>
2021-04-27 20:08:43 +0530

    some fixes

--- client/src/components/header/LanguageBar.js
+++ client/src/components/header/LanguageBar.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 export default function LanguageBar() {
-  const { i18n } = useTranslation();
+  const { i18n, t } = useTranslation();
 
   return (
     <div className="lg:w-full lg:h-10 bg-blue-800 flex justify-end items-center lg:pr-24 pr-4 text-white">
-      <p className="flex items-center justify-center">Select Language:</p>
+      <p className="flex items-center justify-center">{t('selectLanguage')}</p>
       <button
         className="p-1"
         type="button"
